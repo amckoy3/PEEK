1a60671802eacab834145757f67e49634c780f2e
Ashley Mckoy <you@example.com>
2017-09-25 00:21:23 -0400

    Material icons

--- PEEK/src/app/app.module.ts
+++ PEEK/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MdIconRegistry} from '@angular/material';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdIconModule,
+    BrowserAnimationsModule,
+    MdRaisedButton
   ],
   providers: [],
   bootstrap: [AppComponent]
